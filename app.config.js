0aafd3e26df254eaca969d97a1bb59ff6c7790ea
Badhri Hari <114570849+badhri-hari@users.noreply.github.com>
2025-05-21 07:39:03 +0530

    backend fixes

--- app.config.js
+++ app.config.js
@@ -5,8 +5,17 @@ export default {
     name: "Recignize",
     slug: "recignize",
     version: "1.0.0",
+    updates: {
+      url: "https://u.expo.dev/9574ac96-ff47-4326-8e0c-028ffe7d9a10",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
     extra: {
       REACT_NATIVE_BACKEND_URL: process.env.REACT_NATIVE_BACKEND_URL,
+      eas: {
+        projectId: "9574ac96-ff47-4326-8e0c-028ffe7d9a10",
+      },
     },
   },
 };
